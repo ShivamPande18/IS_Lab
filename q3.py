625170a4befa42fdb01ae7ca67064566795c4ee7
Shivam Pande <shivshivampande18@gmail.com>
2024-07-26 11:11:38 +0530

    Create q3.py

---
+++ q3.py
@@ -0,0 +1,27 @@
+message = "i am learning infromation security"
+newmsg=""
+for m in message:
+	if(m==" ") :
+		newmsg+=" "
+	else:
+		i = ord(m) - ord("a");
+		#print(i,end=" ");
+		i = (i*15)%26;
+		i = (i+20)%26;
+		newmsg += chr(i + ord('a'))
+	
+	
+print(newmsg)
+
+key = pow(15,-1,26)
+dec = ""
+
+for m in newmsg:
+	if(m==" ") :
+		dec+=" "
+	else:
+		i = ord(m) - ord("a");
+		i = ((i-20) * key)%26;
+		dec += chr(i + ord('a'))
+		
+print(dec)
