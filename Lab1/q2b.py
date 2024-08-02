c9a9bb3669c7ea0338f6a3255bfee7b502e69eb3
Shivam Pande <shivshivampande18@gmail.com>
2024-08-02 09:29:21 +0530

    Add files via upload

---
+++ Lab1/q2b.py
@@ -0,0 +1,44 @@
+message = "the house is being sold tonight"
+newmsg=""
+dec=""
+autokey = "g"
+key = "g"
+for i in range(len(message)-1): 
+	if(message[i] != " "):
+		key+=message[i]
+cur = 0;
+
+
+for m in message:
+	if(m==" ") :
+		newmsg+=" "
+	else:
+		i = ord(m) - ord("a");
+		#print(i,end=" ");
+		i = ( i + (ord(key[cur]) - ord("a")) ) % 26;
+		cur+=1
+		if(cur>=len(key)): cur=0
+		newmsg += chr(i + ord('a'))
+	
+print(newmsg)
+
+cur=0
+
+for m in newmsg:
+	if(m==" ") :
+		dec+=" "
+	else:
+		i = ord(m) - ord("a");
+	
+		#print(i,end=" ");
+		i = (i- (ord(key[cur]) - ord("a")) )
+		cur+=1
+		if(cur>=len(key)): cur=0
+		if(i<0):
+			i = -i;
+			i = i%26
+			dec += chr(26-i + ord('a'))
+		else: 
+			dec += chr(i + ord('a')) 
+		
+print(dec)
